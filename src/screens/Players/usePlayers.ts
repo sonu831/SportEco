2faffa27b66d5c8a1d7892061e9197a3536a38d2
Ashish-K1 <ashish.k0094@gmail.com>
2023-09-22 21:25:13 +0530

    Update usePlayers.ts

--- src/screens/Players/usePlayers.ts
+++ src/screens/Players/usePlayers.ts
@@ -5,7 +5,11 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../Navigation/types";
 import { RouteProp } from "@react-navigation/native";
 import { AppDispatch } from "../../store";
-import { deletePlayer } from "../../services/players";
+import {
+  deletePlayer,
+  fetchPlayerById,
+  fetchPlayers,
+} from "../../services/players";
 import { playerDetails$ } from "../../store/players/selectors";
 import { PlayerDefinition } from "../../types/player";
 import { UpdateStateRequest } from "../../types/UpdateState";
@@ -14,53 +18,70 @@ import { setIsLoginVerified, setIsVerified } from "../../store/users/reducers";
 import { fetchUserById, getAllStates } from "../../services/users";
 
 type InitialState = {
-    showConfirmation: boolean;
+  showConfirmation: boolean;
+  playerList: any[];
 };
 
 const initialState = {
-    showConfirmation: false,
+  showConfirmation: false,
+  playerList: [],
 };
 
 const usePlayers = ({
-    navigation,
-    route,
+  navigation,
+  route,
 }: {
-    navigation: NativeStackNavigationProp<
-        RootStackParamList,
-        keyof RootStackParamList,
-        undefined
-    >;
-    route: RouteProp<RootStackParamList, "MyAccount">;
+  navigation: NativeStackNavigationProp<
+    RootStackParamList,
+    keyof RootStackParamList,
+    undefined
+  >;
+  route: RouteProp<RootStackParamList, "MyAccount">;
 }) => {
-    const dispatch = useDispatch<AppDispatch>();
-    const userDetails = useSelector(userDetails$);
-    const playerDetails: Partial<PlayerDefinition> = useSelector(playerDetails$);
-    const [state, setState] = useState<Partial<InitialState>>(initialState);
+  const dispatch = useDispatch<AppDispatch>();
+  const userDetails = useSelector(userDetails$);
+  const playerDetails: Partial<PlayerDefinition> = useSelector(playerDetails$);
+  const [state, setState] = useState<Partial<InitialState>>(initialState);
+  const [playerProfileResponse, setPlayerProfileResponse] = useState<any>({});
 
-    const { showConfirmation } = state;
+  const { showConfirmation } = state;
 
-    const updateState = (request: UpdateStateRequest<keyof InitialState>) => {
-        if (Array.isArray(request)) {
-            request.forEach(({ key, value }) =>
-                setState((preState) => ({ ...preState, [key]: value }))
-            );
-        } else {
-            const { key, value } = request;
-            setState((preState) => ({ ...preState, [key]: value }));
-        }
-    };
+  useEffect(() => {
+    if (!route?.params?.id) return;
+    dispatch(fetchPlayerById(route.params.id)).then((res) => {
+      console.log("=====res=====", res?.payload?.data);
+      setPlayerProfileResponse(res?.payload?.data);
+    });
+  }, []);
 
-    const handleGoBack = () => navigation.goBack();
+  const updateState = (request: UpdateStateRequest<keyof InitialState>) => {
+    if (Array.isArray(request)) {
+      request.forEach(({ key, value }) =>
+        setState((preState) => ({ ...preState, [key]: value }))
+      );
+    } else {
+      const { key, value } = request;
+      setState((preState) => ({ ...preState, [key]: value }));
+    }
+  };
 
+  const handleGoBack = () => navigation.goBack();
 
-    const handleEditBtn = () => navigation.navigate("MyAccount");
-    return {
-        userDetails,
-        handleGoBack,
-        handleEditBtn,
-        updateState,
-        state,
-    };
+  useEffect(() => {
+    dispatch(fetchPlayers()).then((res) =>
+      updateState({ key: "playerList", value: res?.payload?.data ?? [] })
+    );
+  }, [dispatch]);
+
+  const handleEditBtn = () => navigation.navigate("MyAccount");
+  return {
+    userDetails,
+    handleGoBack,
+    handleEditBtn,
+    updateState,
+    state,
+    playerProfileResponse,
+  };
 };
 
 export default usePlayers;
