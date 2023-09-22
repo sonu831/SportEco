fcecc0c78186429441c4b2f2cfd6baa1e5bbbb75
Ashish-K1 <ashish.k0094@gmail.com>
2023-09-22 21:24:26 +0530

    Update CreatePlayer.tsx

--- src/screens/Players/CreatePlayer.tsx
+++ src/screens/Players/CreatePlayer.tsx
@@ -1,135 +1,192 @@
-import { StyleSheet, View, Image, TouchableOpacity, ScrollView } from 'react-native'
-import React from 'react'
-import Header from '../../components/MyHeader'
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import Header from "../../components/MyHeader";
 import Feather from "react-native-vector-icons/Feather";
 import { TextInput } from "react-native-paper";
-import MyText from '../../components/MyText';
+import MyText from "../../components/MyText";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { CenteredLineWithText, PhoneNumberInput, SelectionComponent } from '../../components';
-import { Colors } from '../../constants/Colors';
-
+import {
+  CenteredLineWithText,
+  PhoneNumberInput,
+  SelectionComponent,
+} from "../../components";
+import { Colors } from "../../constants/Colors";
+import { addPlayer } from "../../services/players";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store";
 
 const CreatePlayer = ({ navigation }) => {
-    const gotoPlayerProfile = () => navigation.navigate('PlayerProfile')
-    const options = [
-        { label: "Male", icon: "male" },
-        { label: "Female", icon: "female-sharp" },
-        { label: "Other", icon: "add" },
-    ];
-    const [firstName, setFirstName] = React.useState("")
-    const [lastName, setLastName] = React.useState("")
-    const [dob, setDob] = React.useState("")
-    const [phoneNumber, setPhoneNumber] = React.useState("")
-    const [selectedOption, setSelectedOption] = React.useState(options[0].label);
+  const gotoPlayerProfile = (id: string) =>
+    navigation.navigate("PlayerProfile", { id });
+  const options = [
+    { label: "Male", icon: "male" },
+    { label: "Female", icon: "female-sharp" },
+    { label: "Other", icon: "add" },
+  ];
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [dob, setDob] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [selectedOption, setSelectedOption] = React.useState(options[0].label);
+  const dispatch = useDispatch<AppDispatch>();
 
-    const handleOptionPress = (option) => {
-        setSelectedOption(option)
+  const handleOptionPress = (option) => {
+    setSelectedOption(option);
+  };
+
+  const handleCreaterPlayer = () => {
+    const request = {
+      first_name: firstName,
+      last_name: lastName,
+      dbo: {
+        date: "27",
+        month: "2",
+        year: "1990",
+      },
+      gender: selectedOption,
+      phonenumber: phoneNumber,
+      avatarimage: 10,
     };
-    return (
-        <View style={styles.container}>
-            <Header
-                title='Create Player'
-                hasActionIcon
-                actionBtnPress={gotoPlayerProfile}
-                ActionIcon={<Feather name="check" size={18} color={"#fff"}
-
-                />}
-                backgroundColor={"#FBF1D8"}
-            />
-            <View
-                style={{
-                    backgroundColor: "#FBF1D8",
-                    padding: 25,
-                    borderBottomLeftRadius: 70,
-                    borderBottomRightRadius: 70,
-                }}
-            >
-                <Image
-                    source={require("../../assets/images/group904.png")}
-                    style={{
-                        alignSelf: "center",
-                        borderWidth: 2,
-                        borderColor: Colors.darkslategray,
-                        borderRadius: 100
-                    }}
+    dispatch(addPlayer({ data: request })).then((res) => {
+      const resData = res.payload?.data;
+      console.log("=====resData====", resData);
+      gotoPlayerProfile(res.payload?.data._id);
+    });
+  };
+  return (
+    <View style={styles.container}>
+      <Header
+        title="Create Player"
+        hasActionIcon
+        actionBtnPress={handleCreaterPlayer}
+        ActionIcon={<Feather name="check" size={18} color={"#fff"} />}
+        backgroundColor={"#FBF1D8"}
+        isActionBtnDisabled={
+          !firstName || !lastName || !dob || !selectedOption || !phoneNumber
+        }
+      />
+      <View
+        style={{
+          backgroundColor: "#FBF1D8",
+          padding: 25,
+          borderBottomLeftRadius: 70,
+          borderBottomRightRadius: 70,
+        }}
+      >
+        <Image
+          source={require("../../assets/images/group904.png")}
+          style={{
+            alignSelf: "center",
+            borderWidth: 2,
+            borderColor: Colors.darkslategray,
+            borderRadius: 100,
+          }}
+        />
+      </View>
+      <View style={styles.mainView}>
+        <TextInput
+          mode="outlined"
+          label="First Name"
+          placeholder="First Name"
+          activeOutlineColor="grey"
+          placeholderTextColor={"#000"}
+          value={firstName}
+          onChangeText={(text) => setFirstName(text)}
+        />
+        <View style={{ height: "3%" }} />
+        <TextInput
+          mode="outlined"
+          label="Last Name"
+          placeholder="Last Name"
+          activeOutlineColor="grey"
+          placeholderTextColor={"#000"}
+          value={lastName}
+          onChangeText={(text) => setLastName(text)}
+        />
+        <View style={{ height: "3%" }} />
+        <PhoneNumberInput
+          phoneNumber={phoneNumber}
+          onChangePhoneNumber={(phone: string) => {
+            setPhoneNumber(phone);
+          }}
+        />
+        <View style={{ height: "3%" }} />
+        <TextInput
+          mode="outlined"
+          label="Date of Birth"
+          placeholder="Date of Birth"
+          activeOutlineColor="grey"
+          placeholderTextColor={"#000"}
+          value={dob}
+          onChangeText={(text) => setDob(text)}
+        />
+        <CenteredLineWithText lineText="Gender" />
+        <View
+          style={{
+            flexDirection: "row",
+            alignContent: "center",
+            justifyContent: "center",
+          }}
+        >
+          {options.map((option, index) => (
+            <View key={index}>
+              <TouchableOpacity
+                onPress={() => handleOptionPress(option.label)}
+                style={[
+                  styles.option,
+                  selectedOption === option.label && styles.selectedOption,
+                ]}
+              >
+                <Ionicons
+                  name={option.icon}
+                  size={50}
+                  color={
+                    selectedOption === option.label
+                      ? Colors.darkslategray
+                      : Colors.gray
+                  }
                 />
+                <MyText
+                  text={option.label}
+                  center={true}
+                  color={
+                    selectedOption === option.label
+                      ? Colors.darkslategray
+                      : Colors.gray
+                  }
+                />
+              </TouchableOpacity>
             </View>
-            <View style={styles.mainView}>
-                <TextInput
-                    mode="outlined"
-                    label="First Name"
-                    placeholder="First Name"
-                    activeOutlineColor="grey"
-                    placeholderTextColor={"#000"}
-                    value={firstName}
-                    onChangeText={(text) => setFirstName(text)}
-                />
-                <View style={{ height: "3%" }} />
-                <TextInput
-                    mode="outlined"
-                    label="Last Name"
-                    placeholder="Last Name"
-                    activeOutlineColor="grey"
-                    placeholderTextColor={"#000"}
-                    value={lastName}
-                    onChangeText={(text) => setLastName(text)}
-                />
-                <View style={{ height: "3%" }} />
-                <PhoneNumberInput
-                    phoneNumber={phoneNumber}
-                    onChangePhoneNumber={(phone: string) => {
-                        setPhoneNumber(phone)
-                    }}
-                />
-                <View style={{ height: "3%" }} />
-                <TextInput
-                    mode="outlined"
-                    label="Date of Birth"
-                    placeholder="Date of Birth"
-                    activeOutlineColor="grey"
-                    placeholderTextColor={"#000"}
-                    value={dob}
-                    onChangeText={(text) => setDob(text)}
-                />
-                <CenteredLineWithText lineText="Gender" />
-                <View style={{ flexDirection: 'row', alignContent: 'center', justifyContent: 'center' }}>
-                    {options.map((option, index) => (
-                        <View key={index}>
-                            <TouchableOpacity
-                                onPress={() => handleOptionPress(option.label)}
-                                style={[
-                                    styles.option,
-                                    selectedOption === option.label && styles.selectedOption,
-                                ]}
-                            >
-                                <Ionicons name={option.icon} size={50} color={selectedOption === option.label ? Colors.darkslategray : Colors.gray} />
-                                <MyText text={option.label} center={true} color={selectedOption === option.label ? Colors.darkslategray : Colors.gray} />
-                            </TouchableOpacity>
-                        </View>
-                    ))}
-                </View>
-            </View>
+          ))}
         </View>
-    )
-}
-export default CreatePlayer
+      </View>
+    </View>
+  );
+};
+export default CreatePlayer;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-    },
-    mainView: {
-        padding: 20,
-    },
-    option: {
-        padding: 20,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: "#ccc",
-        marginHorizontal: 5,
-    },
-    selectedOption: {
-        borderColor: "#000",
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  mainView: {
+    padding: 20,
+  },
+  option: {
+    padding: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginHorizontal: 5,
+  },
+  selectedOption: {
+    borderColor: "#000",
+  },
+});
