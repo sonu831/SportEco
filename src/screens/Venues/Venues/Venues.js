dcfb88dcf6151705f85716e899a3ef6bd4285a71
iamvishaldev <vishalyadavdev1@gmail.com>
2023-09-30 11:39:56 +0530

    working on venue screens

---
+++ src/screens/Venues/Venues/Venues.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Venues = () => {
+    return (
+        <View>
+            <Text>Venues</Text>
+        </View>
+    )
+}
+
+export default Venues
+
+const styles = StyleSheet.create({})
