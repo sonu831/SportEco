ed00cbc776fcd04f49b30f416fe08a99710448a8
Yogendra Singh <yogendra.singh@vinted.com>
2023-10-29 14:29:21 +0530

    Show Image on the card

--- src/components/CardItem/index.tsx
+++ src/components/CardItem/index.tsx
@@ -4,7 +4,6 @@ import AntDesign from "react-native-vector-icons/AntDesign";
 import { Colors } from "../../constants/Colors";
 import { styles } from "./styles";
 import group9 from "../../assets/images/group904.png";
-import AvatarImage from "../AvatarImage";
 
 const CardItem = ({
   Title,
@@ -18,24 +17,25 @@ const CardItem = ({
   padding = 10,
   imageSource,
 }) => {
+  const renderImage = () => {
+    if (isImage) {
+      // Check if imageSource is a string (could be base64 or a URI)
+      if (typeof imageSource === "string") {
+        return <Image style={styles.image} source={{ uri: imageSource }} />;
+      }
+      // If imageSource is not a string, fallback to the default image
+      return <Image style={styles.image} source={group9} />;
+    }
+    return null;
+  };
+
   return (
     <TouchableOpacity
       onPress={onPress}
       style={[styles.container, { borderColor, padding }]}
     >
       <View style={styles.innerContainer}>
-        {isImage && (
-          <View style={styles.image}>
-            <AvatarImage
-              height={50}
-              width={50}
-              imageUrl={isImage ? imageSource : ""}
-              placeholderImage={group9}
-              resizeMode="cover"
-              isEdit={false}
-            />
-          </View>
-        )}
+        {renderImage()}
         {Icon}
         <View>
           <Text style={[styles.title, { color: titleColor }]}>{Title}</Text>
